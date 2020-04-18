7a960fabbea046e6135d61d2e6cff0676b85c18d
Jamie Sinclair <jamie.sinclair@houzz.com>
2020-04-18 11:24:09 +0900

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import DropForm from './DropForm';
+import Preview from './Preview';
+
+export default () => {
+    const [file, setFile] = useState(null);
+    return (
+        <div>
+            <h1>Gif Spin</h1>
+            {!file && <DropForm onAddFile={file => setFile(file)} />}
+            {file && <Preview file={file} />}
+        </div>
+    ); 
+};
+
+
+
---
+++ src/components/DropForm.js
@@ -0,0 +1,34 @@
+import { h } from 'preact';
+import { useState, useEffect, useRef } from 'preact/hooks';
+import Ascender from 'ascender';
+import styles from './DropForm.css';
+
+const ascenderOptions = {
+    dropArea: { 
+        multipleFiles: false
+    }
+};
+
+export default function DropForm({ onAddFile }) {
+    const [ascenderForm, setAscenderForm] = useState(null);
+    const formRef = useRef(null);
+    useEffect(() => {
+        if (!ascenderForm && formRef.current) {
+            const instance = new Ascender(formRef.current, ascenderOptions);
+            instance.on('file:added', onAddFile);
+            setAscenderForm(instance);
+        }
+        return () => {
+            if (ascenderForm) {
+                ascenderForm.destroy();
+                setAscenderForm(null);
+            }
+        }
+    }, [formRef.current])
+
+    return (
+        <form class={styles.DropForm} ref={formRef}>
+            I am a form
+        </form>
+    )
+}
