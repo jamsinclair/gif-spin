0915f7767ac17aa5393f9991d2836c65d011adf8
N1ck <nicholas.o.lockhart@gmail.com>
2020-04-20 12:54:54 +1200

    Fix more xo formatting

--- src/createSpinGif.js
+++ src/createSpinGif.js
@@ -1,20 +1,20 @@
-import GIF from "gif.js/dist/gif";
-import rotateImage from "./rotateImage";
+import GIF from 'gif.js/dist/gif';
+import rotateImage from './rotateImage';
 
 export default async function createSpinningGif(
 	src,
 	{duration = 1500, fps = 14, quality = 10, showFullImage, showAntiClockwise}
 ) {
-  const gif = new GIF({
-    transparent: "#000",
-    workers: 2,
-    // Parcel uses file hash in file names
-    // Use this to reference worker file correctly so we can load it
-    // @note will need to update name if the script content updates
-    workerScript: "gif.worker.ecec0195.js",
-    quality,
-    differ: "FloydSteinberg-serpentine",
-  });
+	const gif = new GIF({
+		transparent: '#000',
+		workers: 2,
+		// Parcel uses file hash in file names
+		// Use this to reference worker file correctly so we can load it
+		// @note will need to update name if the script content updates
+		workerScript: 'gif.worker.ecec0195.js',
+		quality,
+		differ: 'FloydSteinberg-serpentine'
+	});
 
 	const delay = duration / fps;
 	const frames = Math.round((duration / 1000) * fps);
@@ -22,31 +22,31 @@ export default async function createSpinningGif(
 		const rotation = ((i + 1) / frames) * 360;
 		// eslint-disable-next-line no-await-in-loop
 		const imageCtx = await rotateImage(
-      src,
-      rotation,
-      showFullImage,
-      showAntiClockwise
-    );
+			src,
+			rotation,
+			showFullImage,
+			showAntiClockwise
+		);
 		if (!gif.options.width || !gif.options.height) {
 			gif.options.width = imageCtx.canvas.width;
 			gif.options.height = imageCtx.canvas.height;
 		}
 
-    gif.addFrame(imageCtx, { copy: true, delay });
-  }
+		gif.addFrame(imageCtx, {copy: true, delay});
+	}
 
-  const result = new Promise((resolve, reject) => {
-    gif.on("abort", function () {
-      reject(new Error("Gif creation aborted"));
-    });
-    gif.on("finished", function (blob) {
-      resolve(URL.createObjectURL(blob));
-    });
-    gif.render();
-  });
+	const result = new Promise((resolve, reject) => {
+		gif.on('abort', function () {
+			reject(new Error('Gif creation aborted'));
+		});
+		gif.on('finished', function (blob) {
+			resolve(URL.createObjectURL(blob));
+		});
+		gif.render();
+	});
 
-  return {
-    abort: () => gif.abort(),
-    result,
-  };
+	return {
+		abort: () => gif.abort(),
+		result
+	};
 }
