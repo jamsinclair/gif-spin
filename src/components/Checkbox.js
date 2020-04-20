a32801942f1b10bea7340575a6a80dead3a660a4
jamsinclair <jamsinclairnz@gmail.com>
2020-04-20 09:20:42 +0900

    Add checkbox component

---
+++ src/components/Checkbox.js
@@ -0,0 +1,17 @@
+import {h} from 'preact';
+import styles from './Checkbox.css';
+
+export default function Checkbox({label, value, onChange}) {
+	return (
+		<label class={styles.Checkbox}>
+			<input
+				type="checkbox"
+				checked={value}
+				onChange={(event) => {
+          onChange(event.target.checked)
+        }}
+			/>
+			{label}
+		</label>
+	);
+}
